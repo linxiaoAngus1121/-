65edc6f6aa283f9c7da0757b779743de023aabcc
linxiaoAngus1121 <Angus1121@126.com>
2018-04-01 16:09:11 +0800

    ”第一次提交“

---
+++ app/src/main/java/cn/my/forward/ChoiseActivity.java
@@ -0,0 +1,69 @@
+package cn.my.forward;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import cn.my.forward.adapter.MyAdapter;
+
+public class ChoiseActivity extends AppCompatActivity {
+
+    private Intent mIntent;
+    private GridView mGridView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choise);
+        mGridView = (GridView) findViewById(R.id.choise_grid);
+        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("000", "当前的postion" + position);
+                switch (position) {
+                    case 0:
+                        mIntent = new Intent(ChoiseActivity.this, SourceQueryActivity.class);
+                        break;
+                    case 1:
+                        mIntent = new Intent(ChoiseActivity.this, TimeTableActivity.class);
+                        break;
+                    default:
+                        break;
+                }
+                startActivity(mIntent);
+            }
+        });
+        initAdapter();
+    }
+
+    private void initAdapter() {
+
+        MyAdapter adapter = new MyAdapter(ChoiseActivity.this);
+        mGridView.setAdapter(adapter);
+    }
+
+
+    /**
+     * 课表查询
+     *
+     * @param view 视图
+     */
+    public void toTable(View view) {
+        mIntent = new Intent(ChoiseActivity.this, TimeTableActivity.class);
+        startActivity(mIntent);
+    }
+
+    /**
+     * 成绩查询
+     *
+     * @param view 视图
+     */
+    public void toScore(View view) {
+        mIntent = new Intent(ChoiseActivity.this, SourceQueryActivity.class);
+        startActivity(mIntent);
+    }
+}
---
+++ app/src/main/java/cn/my/forward/adapter/MyAdapter.java
@@ -0,0 +1,79 @@
+package cn.my.forward.adapter;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.my.forward.R;
+
+/**
+ * Created by 123456 on 2018/3/19.
+ */
+
+public class MyAdapter extends BaseAdapter {
+
+    private String[] title = {"成绩查询", "课表查询", "考试查询", "4，6级", "社团活动", "密码修改", "校园招聘", "火车汽车票",
+            "景点门票"
+    };
+    private List<Drawable> picture = new ArrayList<>();
+    private LayoutInflater from;
+
+
+    public MyAdapter(Context context) {
+        from = LayoutInflater.from(context);
+        picture.add(context.getResources().getDrawable(R.drawable.picture));
+        picture.add(context.getResources().getDrawable(R.drawable.pencil));
+        picture.add(context.getResources().getDrawable(R.drawable.calendar));
+        picture.add(context.getResources().getDrawable(R.drawable.four_six));
+        picture.add(context.getResources().getDrawable(R.drawable.activity));
+        picture.add(context.getResources().getDrawable(R.drawable.key));
+        picture.add(context.getResources().getDrawable(R.drawable.job));
+        picture.add(context.getResources().getDrawable(R.drawable.train));
+        picture.add(context.getResources().getDrawable(R.drawable.ticket));
+
+    }
+
+    @Override
+    public int getCount() {
+        return title.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return picture.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = from.inflate(R.layout.adapter_grid_item, parent, false);
+            holder.mTextView = (TextView) convertView.findViewById(R.id.grid_item);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.mTextView.setText(title[position]);
+        holder.mTextView.setCompoundDrawablesWithIntrinsicBounds(null, picture.get(position),
+                null, null);
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        TextView mTextView;
+    }
+}
